860369a067de852bc76668252a1cc4195a64e41b
Chema <jose.garcia-espada@gft.com>
2021-09-11 20:25:53 +0200

    remove dist

--- dist/index.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const crawler_1 = require("./crawler");
-exports.Crawler = crawler_1.Crawler;
-//# sourceMappingURL=index.js.map
